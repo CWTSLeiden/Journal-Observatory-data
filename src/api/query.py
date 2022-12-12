f6671b50e3211bd4f1326f62c41db1962e7975ea
Bram van den Boomen <bram.boomen@gmail.com>
2022-12-12 18:01:47 +0100

    cleanup

--- src/api/query.py
+++
@@ -1,7 +0,0 @@
-from utils.query import get_single_result
-
-
-def pad_count(db, query="{?pad a ppo:PAD}") -> int:
-    query = f"select (count(*) as ?count) where {query}"
-    total = get_single_result(db.query(query))
-    return int(total or 0)
--- src/api/utils.py
+++
@@ -1,5 +0,0 @@
-from rdflib.graph import ConjunctiveGraph
-from flask import render_template, request, jsonify
-from api import api
-
-
