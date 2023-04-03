1a6de2c73fe24c38a9a5c0ac27a916bc221341e2
Bram van den Boomen <bram.boomen@gmail.com>
2023-04-03 14:25:22 +0200

    update to new database location

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     debug = job_config.getboolean("main", "debug", fallback=False)
     pad_db = graphdb_setup(pad_config, "pad", recreate=True)
     job_db = graphdb_setup(job_config, "job", recreate=True)
-    convert_all(pad_db, debug)
-    store_pads(pad_db, job_db, debug)
+    convert_all(pad_db, debug=debug)
+    store_pads(pad_db, job_db, debug=debug)
